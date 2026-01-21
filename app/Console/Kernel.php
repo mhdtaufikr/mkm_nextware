fb0584bd4be4d9d0c4727bcf996b87c05ccd3556
Muhammad Taufik <MKM02120@ptmkm.co.id>
2026-01-21 11:13:34 +0700

    add log api

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,16 +17,38 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inventory:sync')
             ->everyFiveMinutes()
-            ->withoutOverlapping();
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/inventory_sync.log'))
+            ->onSuccess(function () {
+                \Log::info('✅ inventory:sync SUCCESS at ' . now());
+            })
+            ->onFailure(function () {
+                \Log::error('❌ inventory:sync FAILED at ' . now());
+            });
 
         $schedule->command('inventory-item:sync')
             ->everyFiveMinutes()
-            ->withoutOverlapping();
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/inventory_item_sync.log'))
+            ->onSuccess(function () {
+                \Log::info('✅ inventory-item:sync SUCCESS at ' . now());
+            })
+            ->onFailure(function () {
+                \Log::error('❌ inventory-item:sync FAILED at ' . now());
+            });
 
         $schedule->command('order-items:sync')
-        ->everyFiveMinutes()
-        ->withoutOverlapping();
+            ->everyFiveMinutes()
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/order_items_sync.log'))
+            ->onSuccess(function () {
+                \Log::info('✅ order-items:sync SUCCESS at ' . now());
+            })
+            ->onFailure(function () {
+                \Log::error('❌ order-items:sync FAILED at ' . now());
+            });
     }
+
 
     /**
      * Register the commands for the application.
